b9c57d06607310c6ca9d5c322edcc5ffad4e0e9b
sreerag <sreeragvs1812@gmail.com>
2025-10-05 14:10:01 +0530

    Web icon and changes web name - Done

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Disaster Management App",
+      title: "Re-Life",
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       home: const SplashScreen(),
     );
